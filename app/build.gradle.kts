3746f54449f953f3ccf9061169554fe1ed2fbd43
Amaan Khan <ak3325808@gmail.com>
2025-07-20 14:18:49 +0530

    Debug script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,15 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
